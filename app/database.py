01413f2d7a69f043db71ccf89f9427722f19ded6
Nishita Nadimpalli <nn436@njit.edu>
2025-12-01 10:32:13 -0500

    Add Screenshots

--- app/database.py
+++ app/database.py
@@ -1,13 +1,19 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+psycopg2://postgres:postgres@localhost:5432/app_db",
+# Use SQLite locally
+DATABASE_URL = "sqlite:///./test.db"
+
+# If running inside GitHub Actions, use PostgreSQL
+if os.getenv("GITHUB_ACTIONS") == "true":
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 )
 
-engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
